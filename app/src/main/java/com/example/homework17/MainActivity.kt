9b79ad1ca34496811ae8274563308c1b2383f939
Mikle Zanimonetc <z_a_r_k@mail.ru>
2023-01-10 18:56:11 +0300

    hw 17

--- app/src/main/java/com/example/homework17/MainActivity.kt
+++ app/src/main/java/com/example/homework17/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.homework17
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val increaseButton = findViewById<Button>(R.id.button_increase)
+        increaseButton.setOnClickListener { increment() }
+        val resetButton = findViewById<Button>(R.id.button_reset)
+        resetButton.setOnClickListener { reset() }
+    }
+
+    fun increment() {
+        val counterView = findViewById<TextView>(R.id.counter)
+        var counter = counterView.text.toString().toInt()
+        counter++
+        counterView.text = counter.toString()
+    }
+
+    fun reset() {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("count", findViewById<TextView>(R.id.counter).text.toString())
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/homework17/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.example.homework17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        findViewById<TextView>(R.id.result).text = intent.getStringExtra("count")
+    }
+}
